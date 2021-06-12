76fa8a5cb3ad9e0ea0215cab7c3c4414fda6c65b
Alexandria-dr <sasha110druz@gmail.com>
2021-06-12 23:06:53 +0300

    nav burger

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navButton = document.querySelector('.button')
+const navItem = document.querySelectorAll('.nav-item, .burger-item')
+
+navButton.addEventListener('click', e =>{
+    navButton.classList.toggle('active')
+    navItem.forEach(element => {
+        element.classList.toggle('hide')
+    });
+    document.querySelector('.nav__burger').classList.toggle('active')
+})
