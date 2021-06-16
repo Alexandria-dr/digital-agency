97dfd39451cbf7aa6ad06d20f7e4776ad8786e65
Alexandria-dr <sasha110druz@gmail.com>
2021-06-16 15:59:26 +0300

    section: gallery & facts

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ navButton.addEventListener('click', e =>{
     document.querySelector('.nav__burger').classList.toggle('active')
     document.querySelector('body').classList.toggle('lock')
 })
+
+let swiper = new Swiper(".mySwiper", {
+    autoHeight: false,
+    slidesPerView: 1,
+    slidesPerColumn: 2,
+    slidesPerColumnFill: 'row',
+    loop: false,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+            return current + ' — ' + (total); 
+        }
+    },
+    breakpoints: {
+        1100:{
+            slidesPerView:4,
+        },
+        800:{
+            slidesPerView:3,
+        },
+        500:{
+            slidesPerView: 2,
+
+        }
+    }
+  });
